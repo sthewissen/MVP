e34988c57452255b03a6c943c653d2028cb7de9f
Steven Thewissen <github@thewissen.io>
2020-04-06 08:48:18 +0200

    Added extension for later use

---
+++ MVP/Extensions/ModelExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MVP.Models;
+using MvvmHelpers;
+
+namespace MVP.Extensions
+{
+    public static class ModelExtensions
+    {
+        public static IList<Grouping<int, Contribution>> ToGroupedContributions(this ContributionList list)
+        {
+            var result = new List<Grouping<int, Contribution>>();
+            DateTime periodStart = new DateTime(DateTime.Now.Year, 4, 1);
+
+            // If we are before the 1st of April, the period start is last year's.
+            if (DateTime.Now < periodStart)
+            {
+                periodStart = periodStart.AddYears(-1);
+            }
+
+            var thisPeriod = list.Contributions.Where(x => x.StartDate >= periodStart).OrderByDescending(x => x.StartDate);
+            var lastPeriod = list.Contributions.Where(x => x.StartDate < periodStart).OrderByDescending(x => x.StartDate);
+
+            result.Add(new Grouping<int, Contribution>(0, thisPeriod));
+            result.Add(new Grouping<int, Contribution>(1, lastPeriod));
+
+            return result;
+        }
+    }
+}
