f91e2ab165a9e91410833bcda8ad395ecfa53328
Steven Thewissen <github@thewissen.io>
2020-12-26 22:49:03 +0100

    Added onboarding page 2 back in

--- MVP/ViewModels/IntroViewModel.cs
+++ MVP/ViewModels/IntroViewModel.cs
@@ -30,11 +30,12 @@ namespace MVP.ViewModels
                     Title = Translations.onboarding_1_title,
                     Description = Translations.onboarding_1_description
                 },
-                //new OnboardingItem {
-                //    ImageName="onboarding2",
-                //    Title = ,
-                //    Description = "Gather additional insights through your contribution statistics. When were you most active? What type of contributions are you favorite?"
-                //},
+                // Not sure yet if this is going to make it in.
+                new OnboardingItem {
+                    ImageName="onboarding2",
+                    Title = "Detailed statistics",
+                    Description = "Gather additional insights through your contribution statistics. When were you most active? What type of contributions are you favorite?"
+                },
                 new OnboardingItem {
                     ImageName="onboarding3",
                     Title = Translations.onboarding_3_title,
