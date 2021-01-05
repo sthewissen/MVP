69c38ce4e421e4ecd590e05cb899294ac6fe2e49
Steven Thewissen <github@thewissen.io>
2021-01-05 09:47:58 +0100

    Name update

--- MVP/ViewModels/AboutViewModel.cs
+++ MVP/ViewModels/AboutViewModel.cs
@@ -36,7 +36,7 @@ namespace MVP.ViewModels
                 new Acknowledgement { Name = "Rick van Dijk", Type = AcknowledgementTypes.Translator },
                 new Acknowledgement { Name = "Alessandro Caliaro", Type = AcknowledgementTypes.Translator },
                 new Acknowledgement { Name = "Eric Winnington", Type = AcknowledgementTypes.Translator },
-                new Acknowledgement { Name = "pioup", Type = AcknowledgementTypes.Translator }
+                new Acknowledgement { Name = "Laurent Palmont", Type = AcknowledgementTypes.Translator }
             };
         }
 
